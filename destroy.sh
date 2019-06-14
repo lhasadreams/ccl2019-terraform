f477cfba505055093e4ba9033b3629e5ff5370ed
lhasadreams <adrian@chef.io>
2019-06-14 16:12:03 +0100

    update

--- destroy.sh
+++ destroy.sh
@@ -1,8 +1,16 @@
 cd aws
-for loop in {1..3}
+for loop in {1..2}
 do
     terraform workspace select st$loop
 
     # Apply the configuration to the workspace
     terraform destroy -auto-approve
+    
+    # Destroy the Gists
+    cmd="gist -l | grep student$loop | sed 's/ .*//'"
+    gistid=$(eval "$cmd")
+    echo $gistid
+    cmd="gist --delete $gistid"
+    echo $cmd
+    $(eval "$cmd")
 done
--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+mkdir creds
 cd aws
 creds=../credentials.txt
+student=../creds/
+gistlist=../gistlist.txt
 # Create the Student Info File
 echo CCL 2019 Workshop credentials > $creds
-for loop in {1..3}
+echo CCL 2019 Workshop gists > $gistlist
+for loop in {1..2}
 do
+    echo CCL 2019 Workshop credentials for Student $loop> $student/student$loop
     # Create a terraform workspace
     terraform workspace new st$loop
     terraform workspace select st$loop
@@ -20,6 +25,10 @@ do
     echo Student $loop >> $creds
     echo Chef Automate >> $creds
     echo a2-url - $a2url >> $creds
+
+    echo Workshop Document URL - https://bit.ly/2Wu3Mrr >> $student/student$loop
+    echo Chef Automate >> $student/student$loop
+    echo a2-url - $a2url >> $student/student$loop
 
 
     url="\"$a2url/api/v0/auth/teams\""
@@ -42,6 +51,10 @@ do
     echo Password - Cod3Can! >> $creds
     echo Token - $token >> $creds
 
+    echo Username - st$loop >> $student/student$loop
+    echo Password - Cod3Can! >> $student/student$loop
+    echo Token - $token >> $student/student$loop
+
     # Add the new user to the Admin group
     url="\"$a2url/api/v0/auth/teams/$adminTeamId/users\""
     command="curl -X POST $url -H $tokenheader -H $header -H $header2 -d \"{\\\"user_ids\\\": [ \\\"$userId\\\" ]}\" |jq"
@@ -55,9 +68,28 @@ do
     echo Username - centos >> $creds
     echo Password - Cod3Can! >> $creds
 
+    echo Centos Workstation  >> $student/student$loop
+    echo Hostname - $centos  >> $student/student$loop
+    echo Username - centos >> $student/student$loop
+    echo Password - Cod3Can! >> $student/student$loop
+
     echo RHEL7 Node  >> $creds
     echo Hostname - $rhel7  >> $creds
     echo Username - ec2-user >> $creds
     echo Password - Cod3Can! >> $creds
+
+    echo RHEL7 Node  >> $student/st$loop
+    echo Hostname - $rhel7  >> $student/student$loop
+    echo Username - ec2-user >> $student/student$loop
+    echo Password - Cod3Can! >> $student/student$loop
+
     echo ______________________________________ >> $creds
+
+    # Write out the Creds as a secret gist
+    gistcmd="gist -ps $student/student$loop"
+    gisturl=$(eval "$gistcmd")
+    # Save the gist url for the student 
+    echo Student $loop >> $gistlist
+    echo URL = $gisturl >> $gistlist
+    echo ______________________________________ >> $gistlist
 done
